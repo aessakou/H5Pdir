26e5bf10772a945a184cbde11f6426eba3f1c8e7
aessakou <ahmedessakout48@gmail.com>
2025-09-11 14:53:56 +0100

    Adding the chapiter question title

--- MathsQuizz/custom.js
+++ MathsQuizz/custom.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		const container = document.querySelector("body");
 		
 		if (container) {
-			console.log("Check here")
 			// Get the first content block
 			const firstKey = Object.keys(H5PIntegration.contents)[0];
 			const metadata = H5PIntegration.contents[firstKey].metadata;
@@ -19,6 +18,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 					container.insertBefore(heading, container.firstChild);
 				}
+
+				// for (const key in dataa) {
+					// 	// if (Object.hasOwnProperty.call(metadata, key)) { // Important for avoiding inherited properties
+					// 	// 	const value = metadata[key];
+					// 	// 	console.log(`${key}: ${value}`);
+					// 	// }
+					// 	console.log(dataa);
+					// }
+					
+					// console.log(dataa["extraTitle"]);
+				}
+			var dataa = H5PIntegration.contents[firstKey].jsonContent;
+			var questions = document.querySelectorAll(".question-container");
+			if (questions && dataa) {
+				dataa  = JSON.parse(dataa);
+				questions.forEach((question, index) => {
+					// Create a new div for the question number
+					const questionNumberDiv = document.createElement("div");
+					questionNumberDiv.className = "question-chapiter-title";
+					questionNumberDiv.innerText = dataa.questions[index].metadata.extraTitle;
+
+
+					// Insert the question number div at the beginning of the question container
+					question.insertBefore(questionNumberDiv, question.firstChild);
+				});
 			}
 
 			clearInterval(interval); // stop checking
